1d90362a8bfcd01142886335579a7b5bffa45321
avichai321 <77723644+avichai321@users.noreply.github.com>
2024-05-13 21:45:15 +0300

    nginx-script5

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# If the Docker is already runing
-docker stop avicii-nginx || true
-
-# If the Docker is exist
-
-docker rm avicii-nginx || true
-
 #deploy nginx
 
 docker run --name avicii-nginx -d -p 8080:80 nginx:latest
