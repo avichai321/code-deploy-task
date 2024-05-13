51a9f22626245f40c388d3c016609fbc634c4ffd
avichai321 <77723644+avichai321@users.noreply.github.com>
2024-05-13 22:50:33 +0300

    Delete scripts/install_nginx.sh

--- scripts/install_nginx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# If the Docker is already runing
-docker stop avicii-nginx || true
-# If the Docker is exist
-docker rm avicii-nginx || true
-#deploy nginx
-docker run --name avicii-nginx -d -p 80:80 nginx:latest
